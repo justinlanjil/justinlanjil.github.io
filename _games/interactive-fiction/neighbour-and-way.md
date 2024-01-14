2a21de4f7edd2c219ff595cf683795ac8ede767b
Justin Lanjil <justinlanjil@gmail.com>
2024-01-14 12:19:58 -0500

    games index page

---
+++ _games/interactive-fiction/neighbour-and-way.md
@@ -0,0 +1,15 @@
+---
+layout: default
+title: Neighbour & Way
+permalink: /neighbour-and-way
+categories: [game, interactive fiction]
+header_img: assets/img/covers/games/if_neighbour_and_way_159.jpg
+play_url: https://wicked-little-tongue.itch.io/neighbour-and-way
+genres: interactive fiction, horror
+warnings: violence, blood, death
+order: 2
+---
+
+You are living in a gloomy flat in the city. A mysterious woman has just moved in upstairs and though you haven’t seen her, her servant has stopped by for a favour.
+
+They say good fences make good neighbours, but what harm could come from being friendly?
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -9,7 +9,7 @@ var store = [{
         "excerpt":
             " ",
         "categories": [],
-        "date": "2024-01-13",
+        "date": "2024-01-14",
         "tags": [],
         "url": "http://localhost:4000/archive",
         "img": null
@@ -24,62 +24,12 @@ var store = [{
         "url": "http://localhost:4000/20150104_first-post/",
         "img": null
       },{
-        "title": "The Acueduct of Segovia",
-        "subtitle": "20 centuries ago",
-        "excerpt":
-            "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and...",
-        "categories": ["blog","picture"],
-        "date": "2019-01-01",
-        "tags": ["landscape","trips","downtown","project-links","wikipedia","image"],
-        "url": "http://localhost:4000/20190101_imahegf-copy-2/",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Acueducto_Segovia_noche.JPG/1024px-Acueducto_Segovia_noche.JPG"
-      },{
-        "title": "The Acueduct of Segovia",
-        "subtitle": "20 centuries ago",
-        "excerpt":
-            "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and...",
-        "categories": ["blog","picture"],
-        "date": "2019-01-01",
-        "tags": ["landscape","trips","downtown","project-links","wikipedia","image"],
-        "url": "http://localhost:4000/20190101_imahegf-copy-3/",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Acueducto_Segovia_noche.JPG/1024px-Acueducto_Segovia_noche.JPG"
-      },{
-        "title": "The Acueduct of Segovia",
-        "subtitle": "20 centuries ago",
-        "excerpt":
-            "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and...",
-        "categories": ["blog","picture"],
-        "date": "2019-01-01",
-        "tags": ["landscape","trips","downtown","project-links","wikipedia","image"],
-        "url": "http://localhost:4000/20190101_imahegf-copy-4/",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Acueducto_Segovia_noche.JPG/1024px-Acueducto_Segovia_noche.JPG"
-      },{
-        "title": "The Acueduct of Segovia",
-        "subtitle": "20 centuries ago",
-        "excerpt":
-            "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and...",
-        "categories": ["blog","picture"],
-        "date": "2019-01-01",
-        "tags": ["landscape","trips","downtown","project-links","wikipedia","image"],
-        "url": "http://localhost:4000/20190101_imahegf-copy/",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Acueducto_Segovia_noche.JPG/1024px-Acueducto_Segovia_noche.JPG"
-      },{
-        "title": "The Acueduct of Segovia",
-        "subtitle": "20 centuries ago",
-        "excerpt":
-            "The Aqueduct of Segovia (Spanish: Acueducto de Segovia; more accurately, the aqueduct bridge) is a Roman aqueduct in Segovia, Spain. It is one of the best-preserved elevated Roman aqueducts and...",
-        "categories": ["blog","picture"],
-        "date": "2019-01-01",
-        "tags": ["landscape","trips","downtown","project-links","wikipedia","image"],
-        "url": "http://localhost:4000/20190101_imahegf/",
-        "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Acueducto_Segovia_noche.JPG/1024px-Acueducto_Segovia_noche.JPG"
-      },{
         "title": "Markdown cheatsheet",
         "subtitle": "The basic syntax of your page",
         "excerpt":
             "What’s Markdown? Markdown is a lightweight markup language that you can use to add formatting elements to plaintext text documents. Created by John Gruber in 2004, Markdown is now one...",
         "categories": ["tutorial"],
-        "date": "2024-01-13",
+        "date": "2024-01-14",
         "tags": ["starter","syntax","markdown"],
         "url": "http://localhost:4000/cheatsheets/01-markdown-cheat-sheet",
         "img": null
@@ -89,7 +39,7 @@ var store = [{
         "excerpt":
             "What’s kramdown? kramdown supercharges Markdown with some interesting features. kramdown is the default Jekyll Markdown processor. When creating your site with Jekyll, you can use the standard Markdown syntax plus...",
         "categories": ["tutorial"],
-        "date": "2024-01-13",
+        "date": "2024-01-14",
         "tags": ["starter","syntax","kramdown"],
         "url": "http://localhost:4000/cheatsheets/02-kramdown-cheat-sheet",
         "img": null
@@ -99,7 +49,7 @@ var store = [{
         "excerpt":
             " ",
         "categories": [],
-        "date": "2024-01-13",
+        "date": "2024-01-14",
         "tags": [],
         "url": "http://localhost:4000/archive",
         "img": null
@@ -109,58 +59,58 @@ var store = [{
         "excerpt":
             "It was a sunny afternoon and Ned was making his way down the main road at a leisurely pace. He had a bodyguarding contract lined up in a country estate...",
         "categories": ["prose","short story","patron saints"],
-        "date": "2024-01-13",
+        "date": "2024-01-14",
         "tags": [],
-        "url": "http://localhost:4000/stories/alls-fair/",
-        "img": "http://localhost:4000/assets/img/covers/stories/allsfair_cover.jpg"
+        "url": "http://localhost:4000/stories/patron-saints/alls-fair/",
+        "img": "http://localhost:4000/assets/img/covers/stories/ps_alls_fair_159.jpg"
       },{
         "title": "Battlecrier",
         "subtitle": null,
         "excerpt":
             "Albert ‘Bertie’ Horner clutched the small stone tightly to his chest, running his fingers over the familiar grooves and notches that defined its form. Overhead the boom of cannon shot...",
         "categories": ["prose","short story","patron saints"],
-        "date": "2024-01-13",
+        "date": "2024-01-14",
         "tags": [],
-        "url": "http://localhost:4000/stories/battlecrier/",
-        "img": "http://localhost:4000/assets/img/covers/stories/battlecrier_cover.jpg"
-      },{
-        "title": "Lost Lenore",
-        "subtitle": null,
-        "excerpt":
-            "Lord Sterling had taken things a bit far when he had commissioned, with the utmost secrecy, a portrait of his lost lady love. It was a massive painting, a ghastly...",
-        "categories": ["prose","short story","standalone"],
-        "date": "2024-01-13",
-        "tags": [],
-        "url": "http://localhost:4000/stories/lost-lenore/",
-        "img": "http://localhost:4000/assets/img/covers/stories/lostlenore_cover.jpg"
+        "url": "http://localhost:4000/stories/patron-saints/battlecrier/",
+        "img": "http://localhost:4000/assets/img/covers/stories/ps_battlecrier_159.jpg"
       },{
         "title": "Spilt Blood",
         "subtitle": null,
         "excerpt":
             "Ned’s mare had injured her foot stepping on a nasty thorn so it was slow going down the quiet tree lined road. Had he been mounted he may not have...",
         "categories": ["prose","short story","patron saints"],
-        "date": "2024-01-13",
+        "date": "2024-01-14",
         "tags": [],
-        "url": "http://localhost:4000/stories/spilt-blood/",
-        "img": "http://localhost:4000/assets/img/covers/stories/bloodspilt_cover.jpg"
+        "url": "http://localhost:4000/stories/patron-saints/spilt-blood/",
+        "img": "http://localhost:4000/assets/img/covers/stories/ps_blood_spilt.jpg"
+      },{
+        "title": "Lost Lenore",
+        "subtitle": null,
+        "excerpt":
+            "Lord Sterling had taken things a bit far when he had commissioned, with the utmost secrecy, a portrait of his lost lady love. It was a massive painting, a ghastly...",
+        "categories": ["prose","short story","standalone"],
+        "date": "2024-01-14",
+        "tags": [],
+        "url": "http://localhost:4000/stories/standalone/lost-lenore/",
+        "img": "http://localhost:4000/assets/img/covers/stories/s_lost_lenore_159.jpg"
       },{
         "title": "Mary's Hare",
-        "subtitle": "fgdlgd",
+        "subtitle": null,
         "excerpt":
-            "                 Mary's Hare  Mary's Hare is short interactive horror story about a woman and a rabbit, based on the story of Mary Toft.             ",
-        "categories": ["game","interactive-fiction"],
-        "date": "2024-01-13",
+            "Mary’s Hare is short interactive horror story about a woman and a rabbit, based on the story of Mary Toft.  ",
+        "categories": ["game","interactive fiction"],
+        "date": "2024-01-14",
         "tags": [],
         "url": "http://localhost:4000/marys-hare",
-        "img": null
+        "img": "http://localhost:4000/assets/img/covers/games/if_marys_hare_159.jpg"
       },{
         "title": "Neighbour & Way",
-        "subtitle": "fgdlgd",
+        "subtitle": null,
         "excerpt":
-            "Neighbour &amp; Way You are living in a gloomy flat in the city. A mysterious woman has just moved in upstairs and though you haven’t seen her, her servant has...",
-        "categories": ["game","interactive-fiction"],
-        "date": "2024-01-13",
+            "You are living in a gloomy flat in the city. A mysterious woman has just moved in upstairs and though you haven’t seen her, her servant has stopped by for...",
+        "categories": ["game","interactive fiction"],
+        "date": "2024-01-14",
         "tags": [],
         "url": "http://localhost:4000/neighbour-and-way",
-        "img": null
+        "img": "http://localhost:4000/assets/img/covers/games/if_neighbour_and_way_159.jpg"
       }]
